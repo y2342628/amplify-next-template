06eba33f10ff849c223205753e85b78797cf97b9
y2342628 <694484379@qq.com>
2024-05-28 16:06:48 +0800

    add auth and google sign up

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth,secret  } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,17 @@ import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('14813931085-a4b5pqt26ca155bibu9ps0va0hb8egbe.apps.googleusercontent.com'),
+        clientSecret: secret('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'),
+        scopes: ['email']
+      },
+      callbackUrls: [
+        'http://localhost:3000/',
+        "https://main.d3afqg86srkfcd.amplifyapp.com/"
+      ],
+      logoutUrls: ['http://localhost:3000/', "https://main.d3afqg86srkfcd.amplifyapp.com/"],
+    }
   },
 });
